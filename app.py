3cde739e862276a1a217c359d57c89a987db5cc3
mcakyerima <mcakyerima@gmail.com>
2024-01-06 23:47:19 +0100

    Added Product Schema

--- app.py
+++ app.py
@@ -1,8 +1,44 @@
-                        ,,,,,,,,,,,,,,...........................................................................................................................
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+# initialize app
+
+app = Flask(__name__);
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Initialize SQlite db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Init DB
+db = SQLAlchemy(app)
+#init marshmallow
+ma = Marshmallow(app)
+
 # Product Class/Model
 class Product(db.Model):
-    id = db.Column()
-    name = 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
+
+# Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price')
+
+# Init Schema
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
 
 
 if __name__ == '__main__':
